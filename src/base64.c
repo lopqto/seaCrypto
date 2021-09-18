b612cce5d6f15259e5ee712c262c5f91048fef27
lopqto <morpix@protonmail.com>
2021-09-18 19:31:48 +0430

    document format

--- src/base64.c
+++ src/base64.c
@@ -2,8 +2,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include "base64.h"
 
+#include "base64.h"
 
 const char base64_encode_chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 const int mod_table[] = {0, 2, 1};
@@ -14,7 +14,7 @@ void base64_generate_decode_table()
 {
     base64_decode_table = malloc(256);
     for (size_t i = 0; i < 64; i++)
-        base64_decode_table[(unsigned char) base64_encode_chars[i]] = i;
+        base64_decode_table[(unsigned char)base64_encode_chars[i]] = i;
 }
 
 size_t base64_encode_size(size_t inputlen)
@@ -25,30 +25,30 @@ size_t base64_encode_size(size_t inputlen)
 
     if (inputlen % 3 != 0)
         encode_size += 3 - (inputlen % 3);
-    
+
     encode_size /= 3;
     encode_size *= 4;
     return encode_size;
 }
 
-
 size_t base64_decode_size(const char *input)
 {
-	size_t inputlen;
-	size_t decode_size;
+    size_t inputlen;
+    size_t decode_size;
 
-	if (input == NULL)
-		return 0;
+    if (input == NULL)
+        return 0;
 
-	inputlen = strlen(input);
-	decode_size = inputlen / 4 * 3;
+    inputlen = strlen(input);
+    decode_size = inputlen / 4 * 3;
 
-	if (input[inputlen - 1] == '=') decode_size--;
-    if (input[inputlen - 2] == '=') decode_size--;
+    if (input[inputlen - 1] == '=')
+        decode_size--;
+    if (input[inputlen - 2] == '=')
+        decode_size--;
 
-	return decode_size;
+    return decode_size;
 }
-
 
 char *base64_encode(const unsigned char *input, size_t inputlen)
 {
@@ -57,7 +57,6 @@ char *base64_encode(const unsigned char *input, size_t inputlen)
 
     if (input == NULL || inputlen == 0)
         return NULL;
-
 
     encode_size = base64_encode_size(inputlen);
 
@@ -77,7 +76,6 @@ char *base64_encode(const unsigned char *input, size_t inputlen)
         output[j++] = base64_encode_chars[concat >> 2 * 6 & 0x3F];
         output[j++] = base64_encode_chars[concat >> 1 * 6 & 0x3F];
         output[j++] = base64_encode_chars[concat >> 0 * 6 & 0x3F];
-
     }
 
     for (size_t i; i < mod_table[inputlen % 3]; i++)
@@ -86,7 +84,6 @@ char *base64_encode(const unsigned char *input, size_t inputlen)
     return output;
 }
 
-
 unsigned char *base64_decode(const char *input, size_t inputlen)
 {
     size_t decode_size;
@@ -94,7 +91,6 @@ unsigned char *base64_decode(const char *input, size_t inputlen)
 
     if (input == NULL)
         return NULL;
-    
 
     decode_size = base64_decode_size(input);
 
@@ -102,23 +98,25 @@ unsigned char *base64_decode(const char *input, size_t inputlen)
 
     printf("%d\n", decode_size);
 
-    if (base64_decode_table == NULL) base64_generate_decode_table();
-    
+    if (base64_decode_table == NULL)
+        base64_generate_decode_table();
 
-    for (size_t i = 0, j = 0; i < inputlen;) 
+    for (size_t i = 0, j = 0; i < inputlen;)
     {
         uint32_t sextet_one = input[i] == '=' ? 0 & i++ : base64_decode_table[input[i++]];
         uint32_t sextet_two = input[i] == '=' ? 0 & i++ : base64_decode_table[input[i++]];
         uint32_t sextet_three = input[i] == '=' ? 0 & i++ : base64_decode_table[input[i++]];
         uint32_t sextet_four = input[i] == '=' ? 0 & i++ : base64_decode_table[input[i++]];
 
-        uint32_t concat = (sextet_one  << 3 * 6) + (sextet_two << 2 * 6) + (sextet_three << 1 * 6) + sextet_four;
+        uint32_t concat = (sextet_one << 3 * 6) + (sextet_two << 2 * 6) + (sextet_three << 1 * 6) + sextet_four;
 
-        if (j < decode_size) output[j++] = (concat >> 2 * 8) & 0xFF;
-        if (j < decode_size) output[j++] = (concat >> 1 * 8) & 0xFF;
-        if (j < decode_size) output[j++] = (concat >> 0 * 8) & 0xFF;
+        if (j < decode_size)
+            output[j++] = (concat >> 2 * 8) & 0xFF;
+        if (j < decode_size)
+            output[j++] = (concat >> 1 * 8) & 0xFF;
+        if (j < decode_size)
+            output[j++] = (concat >> 0 * 8) & 0xFF;
     }
 
     return output;
-
 }
--- src/rc4.c
+++ src/rc4.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
 #include "rc4.h"
 
-#define rc4_key_size 256 
+#define rc4_key_size 256
 
-void swap(unsigned char *a, unsigned char *b) {
+void swap(unsigned char *a, unsigned char *b)
+{
     int tmp = *a;
     *a = *b;
     *b = tmp;
@@ -15,17 +17,19 @@ void swap(unsigned char *a, unsigned char *b) {
 Key-scheduling algorithm (KSA): The key-scheduling algorithm is used to initialize the permutation in the array "S".
 "keylength" is defined as the number of bytes in the key and can be in the range 1 ≤ keylength ≤ 256.
 */
-unsigned char * KSA(char *secret) {
-    
+unsigned char *KSA(char *secret)
+{
+
     unsigned char *S = malloc(rc4_key_size);
 
     int len = strlen(secret);
     int j = 0;
 
-    for(int i = 0; i < rc4_key_size; i++)
+    for (int i = 0; i < rc4_key_size; i++)
         S[i] = i;
 
-    for(int i = 0; i < rc4_key_size; i++) {
+    for (int i = 0; i < rc4_key_size; i++)
+    {
         j = (j + S[i] + secret[i % len]) % rc4_key_size;
 
         swap(&S[i], &S[j]);
@@ -37,14 +41,16 @@ unsigned char * KSA(char *secret) {
 /*
 Pseudo-random generation algorithm (PRGA): For as many iterations as are needed, the PRGA modifies the state and outputs a byte of the keystream.
 */
-unsigned char * PRGA(unsigned char *S, char *plaintext) {
+unsigned char *PRGA(unsigned char *S, char *plaintext)
+{
 
     int i = 0;
     int j = 0;
 
     unsigned char *encryptedtext = malloc(sizeof(int) * strlen(plaintext));
 
-    for(size_t n = 0, len = strlen(plaintext); n < len; n++) {
+    for (size_t n = 0, len = strlen(plaintext); n < len; n++)
+    {
         i = (i + 1) % rc4_key_size;
         j = (j + S[i]) % rc4_key_size;
 
@@ -52,17 +58,16 @@ unsigned char * PRGA(unsigned char *S, char *plaintext) {
         int rnd = S[(S[i] + S[j]) % rc4_key_size];
 
         encryptedtext[n] = rnd ^ plaintext[n];
-
     }
 
     return encryptedtext;
 }
 
-unsigned char * rc4_encrypt(char *secret, char *plaintext) {
+unsigned char *rc4_encrypt(char *secret, char *plaintext)
+{
 
     unsigned char *S = malloc(rc4_key_size);
     S = KSA(secret);
 
     return PRGA(S, plaintext);
-
 }
